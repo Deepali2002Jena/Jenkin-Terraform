28a4340e0e3e1e849758cc28bbfa064a407cf459
Deepali2002Jena <jenadeepali47@gmail.com>
2025-01-02 10:31:57 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,8 +2,8 @@ provider "aws"{
     region= "us-east-1"
 }
 resource "aws_instance" "web-data" {
-  ami           = "ami-0fd05997b4dff7aac"
-  key_name   = "new-key-ssh-http"
+  ami           = "ami-01816d07b1128cd2d"
+  key_name   = "new-key-ssh-http-https"
   instance_type = "t2.micro"
   tags = {
     Name = "ServerA" 
